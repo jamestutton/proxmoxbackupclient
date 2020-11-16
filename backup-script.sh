9f75de510d004e0016e2bc2466682ea1e1d701f8
Johannes <johannes+develop+github@braun-rheingau.de>
2020-11-16 12:33:51 +0100

    Delete backup-script.sh

--- backup-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PBS_PASSWORD=yourPassword
-export PBS_REPOSITORY=yourUsername@pbs@yourHostname:yourDatastore
-export PBS_FINGERPRINT=yourFingerprint
-proxmox-backup-client backup root.pxar:/host-rootfs/
